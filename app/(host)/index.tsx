b314b2eb965db3e13ddb080a79b6630c72ca1392
white flower <114360170+dongBE2k5@users.noreply.github.com>
2025-11-20 01:19:35 +0700

    update

--- app/(host)/index.tsx
+++ app/(host)/index.tsx
@@ -16,6 +16,10 @@ import { HostStack } from "@/types/navigation";
 import BookingDetail from "@/components/employee_staff/screen/bookingDetail";
 import Checkout from "@/components/employee_staff/screen/checkOut";
 import { useFocusEffect } from "expo-router";
+import CCCDScannerScreen from '@/components/host/screen/CCCDScannerScreen';
+import CameraCaptureScreen from '@/components/host/screen/CameraCaptureView';
+import KycFormScreen from "@/components/host/screen/CCCDScannerScreen";
+// import StaffListHotel from "@/components/host/screen/employee/ListStaffHotel";
 
 const Stack = createStackNavigator<HostStack>();
 
@@ -33,11 +37,7 @@ useFocusEffect(
           setLoading(false);
           return;
         }
-          // const role = await AsyncStorage.getItem('role');
-        // if(role!=="ROLE_HOST"){
-        //   return
-        // }
-        
+
         const response = await getHostByUser(Number(userId));
         console.log("📡 getHostByUser response:", response.data);
 
@@ -71,16 +71,16 @@ useFocusEffect(
   return (
     <View style={styles.container}>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <>
-            <Stack.Screen name="HotelList" component={HotelList} options={{ headerShown: true, title: 'Danh sách khách sạn' }}  />
+      {/* <>
+            <Stack.Screen name="HotelList" component={HotelList} />
             <Stack.Screen name="hostBookings" component={HostBookings} />
             <Stack.Screen name="hotelEdit" component={EditHotel} />
             <Stack.Screen name="CreateHotel" component={CreateHotel} />
             <Stack.Screen name="bookingDetail" component={BookingDetail} />
             <Stack.Screen name="checkout" component={Checkout} />
-          </>
-        {/* {isHost ? (
-          // 🏨 Nếu là host => hiển thị các màn hình quản lý khách sạn
+          </> */}
+        {isHost ? (
+// 🏨 Nếu là host => hiển thị các màn hình quản lý khách sạn
           <>
             <Stack.Screen name="HotelList" component={HotelList} />
             <Stack.Screen name="hostBookings" component={HostBookings} />
@@ -88,14 +88,17 @@ useFocusEffect(
             <Stack.Screen name="CreateHotel" component={CreateHotel} />
             <Stack.Screen name="bookingDetail" component={BookingDetail} />
             <Stack.Screen name="checkout" component={Checkout} />
+            {/* <Stack.Screen name="ListStaffHotel" component={StaffListHotel} /> */}
+            
           </>
         ) : (
           // 🧾 Nếu chưa là host => hiển thị quy trình KYC
           <>
-  
+            <Stack.Screen name="CCCDScannerScreen" component={CCCDScannerScreen} />
+            <Stack.Screen name="CameraCaptureView" component={CameraCaptureScreen} />
             <Stack.Screen name="KycFormScreen" component={KycFormScreen} />
           </>
-        )} */}
+        )}
       </Stack.Navigator>
     </View>
   );
--- constants/BaseURL.ts
+++ constants/BaseURL.ts
@@ -4,7 +4,7 @@ import * as Linking from "expo-linking";
 const linkingUrl = Linking.createURL("/");
 
 
-let ip = "192.168.68.105"; // fallback mặc định
+let ip = "192.168.1.6"; // fallback mặc định
 
 try {
     const parsed = new URL(linkingUrl);
@@ -16,19 +16,19 @@ try {
 
     } else {
         // ⚙️ fallback IP LAN của bạn (hoặc để localhost)
-        ip = "192.168.68.105";
+        ip = "192.168.1.6";
     }
 } catch (error) {
     console.warn("Invalid Linking URL, fallback to IP LAN:", error);
-    ip = "192.168.68.105";
+    ip = "192.168.1.6";
 }
 
 const BaseUrl = `http://${ip}:8080/api`;
-//export const BaseUrl2 = `http://${ip}:8080/api`;
-//export const BaseUrl = `https://prudish-leonie-noncapriciously.ngrok-free.dev/api`;
+export const BaseUrl2 = `http://${ip}:8080/api`;
+// export const BaseUrl = `https://prudish-leonie-noncapriciously.ngrok-free.dev/api`;
 
 const urlIP = `http://${ip}:8080`;
- //const urlIP = `https://prudish-leonie-noncapriciously.ngrok-free.dev`;
+//  const urlIP = `https://prudish-leonie-noncapriciously.ngrok-free.dev`;
 // const getBaseUrl = ip;
 export default  BaseUrl ;
 export const urlImage = `${urlIP}/uploads/`;
